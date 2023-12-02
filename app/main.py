7aa9a9d726b7441a94373591a9d3d52e30245cae
Dhominicx <dhominibp4@gmail.com>
2023-12-01 21:02:06 -0300

    Modificação do arquivo main.py para utilização do modelo

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from ultralytics import YOLO
+import cv2 as cv
 
 st.set_page_config(page_title="Fruit Identifier", page_icon=":apple:")
 
@@ -7,8 +9,17 @@ st.title('Fruit Identifier :apple: :banana:')
 st.sidebar.title("Test")
 
 # upload da imagem para predicao
-uploaded_image = st.file_uploader("Upload a CSV")
+image = st.file_uploader("Upload a CSV")
 
-git_url = "https://github.com/louilolo/fruit-identifier/tree/main#fruit-identifier-apple-banana"
-st.link_button("GitHub Repository", git_url)
-# st.link_button("Go to gallery", url)
+# Carregar o modelo e fazer a predicao
+model = YOLO("/content/runs/detect/train5/weights/best.pt")
+results = model.predict(source=image)
+
+# Desenhar o retangulo gerado na predicao do modelo
+cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
+
+# Exibir imagem com bounding box da predição
+st.image(img)
+
+github_url = "https://github.com/louilolo/fruit-identifier/tree/main#fruit-identifier-apple-banana"
+st.link_button("GitHub Repository", github_url)
