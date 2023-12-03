57f0f4ca7e04f3e10961498afb32016ed658567a
CAIO <caiotobe@gmail.com>
2023-12-03 12:28:36 -0300

    modelo da página streamlit comentado

--- app/main.py
+++ app/main.py
@@ -2,24 +2,29 @@ import streamlit as st
 from ultralytics import YOLO
 import cv2 as cv
 
-st.set_page_config(page_title="Fruit Identifier", page_icon=":apple:")
+st.set_page_config(page_title="Fruit Identifier", page_icon=":apple:") #definição de titulo e icon 
+#cria uma sidebar que está relacionada ao menu de escolha (isso foi alterado arbitrariamente so para eu conhecer os comandos do streamlit)
+st.sidebar.title('Menu')
+paginaSelecionada = st.sidebar.selectbox('Selecione a fruta que deseja testar a qualidade',['Maçã', 'Banana']) 
 
-st.title('Fruit Identifier :apple: :banana:')
-
-st.sidebar.title("Test")
-
-# upload da imagem para predicao
-image = st.file_uploader("Upload a CSV")
-
-# Carregar o modelo e fazer a predicao
-model = YOLO("/content/runs/detect/train5/weights/best.pt")
-results = model.predict(source=image)
-
-# Desenhar o retangulo gerado na predicao do modelo
-cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
-
-# Exibir imagem com bounding box da predição
-st.image(img)
-
+# condicional para ver qual fruta o usuario escolheu e a partir dai exibir os textos corretos
+if paginaSelecionada == 'Maçã': 
+    st.title('Check the quality of your apple :apple:')
+    # Upload da imagem + formatos
+    img1 = st.file_uploader("Insira o arquivo desejado: ", type=['jpg', 'jpeg', 'png'] )
+    # Verifica se a imagem foi carregada antes de exibi-la
+    if img1 is not None:
+        #exibição das imagens sem bounding boxes
+        st.image(img1)
+        # Carregando o modelo
+        model = YOLO('gest.pt') 
+        results = model.predict(source=img1, save=True)
+        github_url = "https://github.com/louilolo/fruit-identifier/tree/main#fruit-identifier-apple-banana"
+        st.link_button("GitHub Repository", github_url)
+        # esse trecho desenharia os retângulos na imagem (ainda nao entendi muito bem, teriamos que percorrer todo o arquivo gerado pelo modelo para fazer isso?)
+        # cv.rectangle(img 1 ()...()...()...())
+elif paginaSelecionada == 'Banana':
+     st.title('Check the quality of your banana :banana:')
+     #para bananas repetir o mesmo codigo de cima ou usar tudo numa pagina so, tanto faz...
 github_url = "https://github.com/louilolo/fruit-identifier/tree/main#fruit-identifier-apple-banana"
 st.link_button("GitHub Repository", github_url)
